196fe9e2b5c363d88ca8c259281ec53a4bad9947
Fullyoung <598621498@qq.com>
2015-08-31 09:55:11 +0800

    v1.0

--- src/Demo1/HelloGit.java
+++ src/Demo1/HelloGit.java
@@ -3,6 +3,7 @@ package Demo1;
 public class HelloGit {
 	public static void main(String args){
 		System.out.println("This is a Demo for using GitHub");
+		System.out.println("I  do it in my company !");
 	}
 
 }
